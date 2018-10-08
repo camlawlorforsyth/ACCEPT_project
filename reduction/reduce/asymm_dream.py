ed3bf67b40e70775f7e9cc884b89366f31c0a0b4
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2018-10-07 22:00:13 -0500

    Correct asymm calculation as per paper

--- reduction/reduce/asymm_dream.py
+++ reduction/reduce/asymm_dream.py
@@ -1,14 +1,24 @@
-from pycrates import read_file
+# this script assumes Python 2.7 is in use
+
+from pycrates import read_file # allows reading, writing, and changing FITS files. Based on Crates, provided with CIAO
+                               # Can alternatively use 'from astropy.io import fits' and then 'fits.open(...)', but further
+                               # modifications to the code would then be necessary
 import numpy as np
 import math
 import sys
 
+'''
+The calling code used in reduce2.py for this file, is of the form:
+python asymm_dream.py dimension rot.fits background.fits threshed_broad.fits
+argv[-]    argv[0]     argv[1]   argv[2]     argv[3]          argv[4]
+'''
+
 if len(sys.argv) == 5:
-	A = int(sys.argv[1])
-	R = sys.argv[2]
-	B = sys.argv[3]
-	T = sys.argv[4]
-else:
+	A = int(sys.argv[1]) # the one size dimension of the image, where the image is assumed to be square
+	R = sys.argv[2] # the rotated image
+	B = sys.argv[3] # the background image
+	T = sys.argv[4] # the original science image
+else: # this should only be necessary if not using the automated reduction scripts
 	print "input number of pixels"
 	A = input('')
 	print "input path to rotated image"
@@ -19,29 +29,45 @@ else:
 	print "input path to original image"
 	T = raw_input('')
 
-img_r = read_file(R)
-img_t = read_file(T)
-orig_pix_vals_r = img_r.get_image().values
-orig_pix_vals_t = img_t.get_image().values
+img_r = read_file(R) # read the rotated image
+img_t = read_file(T) # read the science image
+orig_pix_vals_r = img_r.get_image().values # get the pixel values from the rotated image
+orig_pix_vals_t = img_t.get_image().values # get the pixel values from the original science image
 
-r=0
-b=0
-t=0
+'''
+# Michael's original code, suspected to be incorrect
 
-if B == 'no':
-	for x in range(0,A):						
-		for y in range(0,A):
-			r += (img_t.get_image().values[x,y] - img_r.get_image().values[x,y])**2	
-			t += 2*(img_t.get_image().values[x,y])**2
+r, b, t = 0, 0, 0 # inital values
 
+if B == 'no': # if there is no background image
+	for x in range(0, A): # loop for every pixel in the square image
+		for y in range(0, A):
+			r += ( img_t.get_image().values[x,y] - img_r.get_image().values[x,y] )**2
+			t += 2*( ( img_t.get_image().values[x,y] )**2 )
 else:
 	img_b = read_file(B)
 	orig_pix_vals_b = img_b.get_image().values
-
-	for x in range(0,A):						
-		for y in range(0,A):
-			r += (img_t.get_image().values[x,y] - img_r.get_image().values[x,y])**2	
-			b += (img_b.get_image().values[x,y])**2 
-			t += 2*((img_t.get_image().values[x,y])**2)
+	for x in range(0, A):
+		for y in range(0, A):
+			r += ( img_t.get_image().values[x,y] - img_r.get_image().values[x,y] )**2
+			b += ( img_b.get_image().values[x,y] )**2
+			t += 2*( ( img_t.get_image().values[x,y] )**2 )
 
 print (r-b)/t
+'''
+
+num = 0
+denom = 0
+
+for x in range(0, A): # loop for every pixel in the square image
+    for y in range (0, A):
+        if B == 'no': # if there is no background image
+            num += ( img_t.get_image().values[x,y] - img_r.get_image().values[x,y] )**2
+            denom += 2*( ( img_t.get_image().values[x,y] )**2 )
+        else:
+            img_b = read_file(B) # read the background image
+            orig_pix_vals_b = img_b.get_image().values # get the pixel values from the background image
+            num += ( img_t.get_image().values[x,y] - img_b.get_image().values[x,y] - img_r.get_image().values[x,y] )**2
+            denom += 2*( ( img_t.get_image().values[x,y] - img_b.get_image().values[x,y] )**2 )
+
+print num/denom
