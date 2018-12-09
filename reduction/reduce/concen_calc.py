cf4c788483bfc4c2d5f201e7428289328b77924f
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2018-12-08 18:54:31 -0600

    Use functions rather than pure script

--- reduction/reduce/concen_calc.py
+++ reduction/reduce/concen_calc.py
@@ -11,6 +11,8 @@ import astropy.units as u
 from astropy.wcs import WCS
 from photutils import aperture_photometry
 from photutils import SkyCircularAperture
+import warnings
+warnings.filterwarnings("ignore") # ignore warnings about WCS unit changes
 
 import sys
 
@@ -19,16 +21,6 @@ The calling code used in reduce2.py for this file, is of the form:
 python concen_calc.py science_image.fits redshift
 argv[-]    argv[0]         argv[1]       argv[2]
 '''
-
-if len(sys.argv) == 2 :
-    file = sys.argv[1]
-    redshift = sys.argv[2]
-else : # this should only be necessary if not using the automated scripts
-    print('Input the path to the original science image:')
-    file = input('')
-    print('Input the redshift to use:')
-    redshift = float(input(''))
-    print('Input confirmed. Continuing with script.')
 
 (nameMain, zz, zz_err, K0, K0_err, K100, K100_err,
      alpha, Tx, Tx_err, Lbol, Lbol_err, LbolUL, LHa, LHa_err,
@@ -45,42 +37,97 @@ else : # this should only be necessary if not using the automated scripts
 # constants
 cosmo = FlatLambdaCDM(H0 = 70, Om0 = 0.3) # specify the cosmology being used
 
-science = fits.open(file) # open the science image
-#science.info()
-header = science[0].header # define the header, in order to get the WCS
-image = science[0].data # create the image that will be used
-science.close()
+#..........................................................................main
+def main() :
+    
+    if len(sys.argv) == 2 :
+        file = sys.argv[1]
+        redshift = sys.argv[2]
+    else : # this should only be necessary if not using the automated scripts
+        file = get_image()
+        redshift = get_redshift()
+        print('\nInput confirmed. Continuing with script.\n')
+    
+    science = fits.open(file) # open the science image
+    #science.info()
+    header = science[0].header # define the header, in order to get the WCS
+    image = science[0].data # create the image that will be used
+    science.close()
+    
+    world_cs = WCS(header)
+    
+    i = int(np.where(zz==redshift)[0]) # requires redshifts in 'zz' be unique
+    RA = Angle(RAs[i], u.hour) # get the RA, Dec, ROIout for this cluster
+    Dec = Angle(Decs[i], u.deg)
+    R_out = ROIout[i]*u.Mpc
+    
+    D_A = cosmo.angular_diameter_distance(redshift)
+    R_max = (R_out/D_A)*(180/np.pi)*u.deg # to get maximum radius in degrees
+    
+    position = SkyCoord(ra=RA, dec=Dec, distance=D_A)
+    aperture = SkyCircularAperture(position, r=R_max)
+    phot_table = aperture_photometry(image, aperture, wcs=world_cs)
+    max_total = phot_table['aperture_sum'][0]
+    
+    CC, CC_err = concentration(max_total, R_max, position, image, world_cs)
+    
+    print('C: {0:g}, C_err: {1:g}'.format(CC, CC_err))
 
-world_cs = WCS(header)
+#.....................................................................get_image
+def get_image() :
+    
+    prompt = ('Input the path to the original science image: ')
+    warning = '\b'
+    while warning : # drop out if empty
+        userIn = input(warning + prompt)
+        warning = ''
+        try :
+            val = ( userIn )
+            if val == None :
+                warning = ("You must have some image. Please try again.\n\n")
+            else :
+                pass
+        except ValueError :
+            warning = ("Error. Please try again.\n\n")   
+    
+    return val
 
-i = int(np.where(zz==redshift)[0]) # requires redshifts in 'zz' to be unique
-RA = Angle(RAs[i], u.hour) # get the RA, Dec, ROIout for this cluster
-Dec = Angle(Decs[i], u.deg)
-R_out = ROIout[i]*u.Mpc
-
-D_A = cosmo.angular_diameter_distance(redshift)
-R_max = (R_out/D_A)*(180/np.pi)*u.deg # to get maximum radius in degrees
-
-position = SkyCoord(ra=RA, dec=Dec, distance=D_A)
-aperture = SkyCircularAperture(position, r=R_max)
-phot_table = aperture_photometry(image, aperture, wcs=world_cs)
-max_total = phot_table['aperture_sum'][0]
+#..................................................................get_redshift
+def get_redshift() :
+    
+    prompt = ('Input the unique redshift to use for analysis: ')
+    warning = '\b'
+    while warning : # drop out if empty
+        userIn = input(warning + prompt)
+        warning = ''
+        try :
+            val = float( userIn )
+            if val == None :
+                warning = ("You must have the redshift. Please try again.\n\n")
+            elif val < 0 :
+                warning = ("Redshifts aren't negative. Please try again.\n\n")
+            else :
+                pass
+        except ValueError :
+            warning = ("Error. Please try again.\n\n") 
+    
+    return val
 
 #........................................................................search
-def search(desired_total) :
+def search(desired_total, R_max, position, image, wcs) :
     
     eps = 1e-3
     
     aperture = SkyCircularAperture(position, r=0.25*R_max)
-    phot_table = aperture_photometry(image, aperture, wcs=world_cs)
+    phot_table = aperture_photometry(image, aperture, wcs=wcs)
     twentyfive_R_max_total = phot_table['aperture_sum'][0]
     
     aperture = SkyCircularAperture(position, r=0.5*R_max)
-    phot_table = aperture_photometry(image, aperture, wcs=world_cs)
+    phot_table = aperture_photometry(image, aperture, wcs=wcs)
     fifty_R_max_total = phot_table['aperture_sum'][0]
     
     aperture = SkyCircularAperture(position, r=0.75*R_max)
-    phot_table = aperture_photometry(image, aperture, wcs=world_cs)
+    phot_table = aperture_photometry(image, aperture, wcs=wcs)
     seventyfive_R_max_total = phot_table['aperture_sum'][0]
     
     if desired_total > seventyfive_R_max_total :
@@ -94,7 +141,7 @@ def search(desired_total) :
     
     while radius <= R_max :
         aperture = SkyCircularAperture(position, r=radius)
-        phot_table = aperture_photometry(image, aperture, wcs=world_cs)
+        phot_table = aperture_photometry(image, aperture, wcs=wcs)
         total = phot_table['aperture_sum'][0] # the value with no units
         
         if abs(desired_total-total) <= eps*max(abs(desired_total),abs(total)) :
@@ -103,15 +150,15 @@ def search(desired_total) :
             radius += R_max/10000
 
 #.................................................................concentration
-def concentration() :
+def concentration(max_total, R_max, position, image, wcs) :
     
-    r_20 = search(0.2*max_total) # find the radius that encompasses 20% of the
-    r_80 = search(0.8*max_total) # emission, and then the same for 80%
+    # find radius that encompasses 20% of the emission, then the same for 80%
+    r_20 = search(0.2*max_total, R_max, position, image, wcs) 
+    r_80 = search(0.8*max_total, R_max, position, image, wcs) 
     
     uncertainty = 5/np.log(10) * ( (R_max/10000)/r_20 + (R_max/10000)/r_80 )
     
     return 5*np.log10(r_80 / r_20), uncertainty
+#..............................................................end of functions
 
-CC, CC_err = concentration()
-
-print('C: {0:g}, C_err: {1:g}'.format(CC, CC_err))
+main()
