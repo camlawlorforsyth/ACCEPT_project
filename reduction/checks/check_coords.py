711ccdbcb99f4e7739e4a90a166dda310790a097
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-03-26 00:03:50 -0500

    Update check_coords.py

--- reduction/checks/check_coords.py
+++ reduction/checks/check_coords.py
@@ -1,63 +1,73 @@
 # this script assumes Python 3.5 is in use
 
+'''
+The calling code used in verify_coords.py for this file, is of the form:
+python checks/check_coords.py 1E_0657-56 complete
+argv[-]       argv[0]         argv[1]    argv[2]
+'''
+
 # imports
 import os
 import sys
 
-'''
-The calling code used in verify_coords.py for this file, is of the form:
-python checks/check_coords.py 1E0657_56 d
-argv[-]       argv[0]         argv[1]   argv[2]
-'''
+cluster = sys.argv[1] # the cluster name, as indicated
+quality = sys.argv[2] # quality flag indicating if enough data is present
 
-clusterName = sys.argv[1]
-bad = sys.argv[2]
+file = open("issues.txt", "a") # open for writing, appending to the end of
+                               # the file if it exists
 
-L = open("good.txt", "a")
-
-if bad == "d":
-    os.chdir(clusterName)
-
-    if ( not os.path.isfile("ds9_fk.reg")):
-        L.write(clusterName + " is labelled correctly, but 'ds9_fk.reg' is missing!\n")
-    else: 
-        f = open("ds9_fk.reg", "r")
-        f.readline() # read the first line of ds9_fk.reg
-        f6 = f.read(6) # read the first 6 characters
-        if (f6 != "global"):
-            L.write(clusterName + "'s ds9_fk.reg file is saved in the wrong coordinates!\n")
-	f.close()
-
-    if ( not os.path.isfile("expcor_mosaic_2/bk.reg")):
-        L.write(clusterName + " is labelled correctly, but 'bk.reg' is missing!\n")
-    else: 
-        f = open("expcor_mosaic_2/bk.reg", "r")
-        f.readline()
-        f6 = f.read(6)
-        if (f6 != "circle"):
-            L.write(clusterName + "'s bk.reg file is saved in the wrong coordinates!\n")
-	f.close()
-
-    if ( not os.path.isfile("expcor_mosaic_2/sources_mod.reg")):
-        L.write(clusterName + " is labelled correctly, but bin2 'sources_mod.reg' is missing!\n")
-    else: 
-        f = open("expcor_mosaic_2/sources_mod.reg", "r")
-        f.readline()
-        f6 = f.read(6)
-        if (f6 != "circle"):
-            L.write(clusterName + "'s bin2 'sources_mod.reg' file is saved in the wrong coordinates!\n")
-	f.close()
-
-    if ( not os.path.isfile("expcor_mosaic/sources_mod.reg")):
-        L.write(clusterName + " is labelled correctly, but bin0.5 'sources_mod.reg' is missing!\n")
-    else: 
-        f = open("expcor_mosaic/sources_mod.reg", "r")
-        f.readline()
-        f6 = f.read(6)
-        if (f6 != "circle"):
-            L.write(clusterName + "'s bin0.5 'sources_mod.reg' file is saved in the wrong coordinates!\n")
-	f.close()
-	
+if quality == "complete" :
+    
+    os.chdir(cluster)
+    
+    if (not os.path.isfile("ds9_fk.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but 'ds9_fk.reg' is missing.\n")
+    else :
+        newfile = open("ds9_fk.reg", "r") # open for reading
+        newfile.readline() # read the first line of ds9_fk.reg
+        f6 = newfile.read(6) # read the first 6 characters
+        if (f6 != "global") :
+            file.write(cluster + "'s ds9_fk.reg file is saved in the wrong " +
+                       "coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("expcor_mosaic_2/bk.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but 'bk.reg' is missing.\n")
+    else :
+        newfile = open("expcor_mosaic_2/bk.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bk.reg file is saved in the wrong " +
+                       "coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("expcor_mosaic_2/sources_mod.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but bin2 'sources_mod.reg' is missing.\n")
+    else :
+        newfile = open("expcor_mosaic_2/sources_mod.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bin2 'sources_mod.reg' file is saved " +
+                       "in the wrong coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("expcor_mosaic/sources_mod.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but bin0.5 'sources_mod.reg' is missing.\n")
+    else :
+        newfile = open("expcor_mosaic/sources_mod.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bin0.5 'sources_mod.reg' file is saved " +
+                       "in the wrong coordinates.\n")
+        newfile.close()
+    
     os.chdir("..")
 
-L.close()
+file.close()
