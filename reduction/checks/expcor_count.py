146e3e15ef5695728c01a7aaf43d904b195c44e7
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-03-25 23:28:59 -0500

    Update expcor_count.py

--- reduction/checks/expcor_count.py
+++ reduction/checks/expcor_count.py
@@ -4,25 +4,27 @@
 import sys
 import os
 
-f = open("good.txt", "a")
+file = open("issues.txt", "a")
 
 '''
 The calling code used in verify_reproj.py for this file, is of the form:
-python checks/expcor_count.py 1E0657_56 3184 5356 5361 0.296 1.1945
-argv[-]       argv[0]         argv[1]   argv[3]...argv[-3] argv[-2] argv[-1]
+python checks/expcor_count.py 1E_0657-56 0.296 1.1945 554 3184 4984 4985 4986 5355 5356 5357 5358 5361
+argv[-]       argv[0]         argv[1]   argv[3] argv[4] ... argv[N]
 '''
 
-length = len(sys.argv) # length = 7 for the example above
-obsnum = length - 4 # obsnum = 7 - 4 = 3, as we can see from above
+length = len(sys.argv) # length = 14 for the example above
+num_obs = length - 4 # num_obs = 14 - 4 = 10, as we can see from above
 cluster = sys.argv[1] # the cluster name, as indicated
 
-if obsnum > 1: # for clusters with more than 1 observation
-    filenum = len(os.listdir(cluster + "/expcor_mosaic_2")) # the number of files present in [cluster_name]/expcor_mosaic_2/
-    if filenum < (obsnum + 1)*3: # flux_obs creates 3 files for every ObsId, plus 3 additional merged files
-        f.write(cluster + " is bad\n")
+if num_obs > 1 : # for clusters with more than 1 observation
+    num_file = len(os.listdir(cluster + "/expcor_mosaic_2")) # the number of
+                            # files present in [cluster_name]/expcor_mosaic_2/
+    if num_file < (num_obs + 1)*3 : # flux_obs creates 3 files for every ObsId,
+                                    # plus 3 additional merged files
+        file.write(cluster + " has an issue.\n")
     else:
-        f.write(cluster + " is good\n")
+        file.write(cluster + " is fine.\n")
 else:
-    f.write(cluster + " is good\n")
+    file.write(cluster + " only has one observation and is therefore fine.\n")
 
-f.close()
+file.close()
