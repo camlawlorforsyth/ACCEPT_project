c00d5f0b1f44225e0f2fe125c90e9f7626ce40cc
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-03-26 00:02:51 -0500

    Update expcor_count.py

--- reduction/checks/expcor_count.py
+++ reduction/checks/expcor_count.py
@@ -1,20 +1,21 @@
 # this script assumes Python 3.5 is in use
 
-# imports
-import os
-import sys
-
-file = open("issues.txt", "a")
-
 '''
 The calling code used in verify_reproj.py for this file, is of the form:
 python checks/expcor_count.py 1E_0657-56 0.296 1.1945 554 3184 4984 4985 4986 5355 5356 5357 5358 5361
 argv[-]       argv[0]         argv[1]   argv[3] argv[4] ... argv[N]
 '''
 
+# imports
+import os
+import sys
+
 length = len(sys.argv) # length = 14 for the example above
 num_obs = length - 4 # num_obs = 14 - 4 = 10, as we can see from above
 cluster = sys.argv[1] # the cluster name, as indicated
+
+file = open("issues.txt", "a") # open for writing, appending to the end of
+                               # the file if it exists
 
 if num_obs > 1 : # for clusters with more than 1 observation
     num_file = len(os.listdir(cluster + "/expcor_mosaic_2")) # the number of
