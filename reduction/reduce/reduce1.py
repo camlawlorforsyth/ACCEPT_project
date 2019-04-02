f0e84727718d00c7ba2ea4b3809ce172bcf72af7
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-04-02 18:57:27 -0500

    Update reduce1.py

--- reduction/reduce/reduce1.py
+++ reduction/reduce/reduce1.py
@@ -1,96 +1,105 @@
 # this script assumes Python 3.5 is in use
 
+'''
+For information regarding how this script is initialized, see the 'README.md'
+file in reduction/README.md.
+
+The calling code used in get_all_data.py for this file, is of the form:
+python reduce/reduce1.py 1E_0657-56 0.296 1.1945 554 3184 4984 4985 4986 5355 5356 5357 5358 5361
+argv[-]    argv[0]        argv[1]  argv[2] argv[3] ... argv[N]
+'''
+
 # imports
-import sys
 import os
-from math import *
+import sys
 
-#to run this file from within your data folder, type in the terminal with ciao running:
- 
-#python reduce1.py [cluster_name] obsid1 obsid2 ... obsidN z R_out
+length = len(sys.argv) # length = 14 for the example above
+cluster = sys.argv[1] # the cluster name, as indicated
+redshift = float(sys.argv[2]) # the given redshift
+Rout = float(sys.argv[3]) # the maximal outer radius that was fit by Cavagnolo+
 
-#Assumes you run this from within your data folder, and that your data folder contains asymm_dream.py, clumpy_dream.py, ggm_combine
+## STEP 0 - CREATE CLUSTER DIRECTORY ##
 
-#this file will direct you through the reduction steps and process to obtain the CAS parameters
-#for each cluster
+os.system("mkdir " + cluster) # create a new directory for the cluster
+os.chdir(cluster) # change directory to the cluster directory
 
-#Record useful information in chandra_stats.txt
+## STEP 1 - OBTAIN DATA FROM THE CHANDRA DATA ARCHIVE ##
 
-#================================== 1. GET DATA FROM ARCHIVE ==================================#
-#You need to start CIAO first (after having installed it, of course). On the command line, type "ciao"
+# http://cxc.harvard.edu/ciao/threads/intro_data/
+# http://cxc.harvard.edu/ciao/threads/archivedownload/
+# http://cxc.harvard.edu/ciao/ahelp/download_chandra_obsid.html
 
-length = len(sys.argv)
-z = float(sys.argv[length-2])
-Rout = float(sys.argv[length-1])
-clusterName = sys.argv[1]
+cmd = "download_chandra_obsid " # download Chandra data 
 
-os.system("mkdir " + clusterName) # create a new directory for the cluster
-os.system("cp -r ggm_combine/ " + clusterName) # copy the ggm_combine/ directory to the new cluster directory
-os.chdir(clusterName) # change directory to the cluster directory
+for i in range(4, length-1) :
+    cmd += str(sys.argv[i]) + "," # creates a comma-separated list of the
+                                  # passed ObsIds
+cmd += str(sys.argv[length-1]) + "-q" # append the last ObsId
 
-cmd = "download_chandra_obsid " # download Chandra data as per http://cxc.harvard.edu/ciao/threads/archivedownload/
-for i in range(2, length - 3):
-	cmd += str(sys.argv[i]) + "," # creates a comma-separated list of the passed ObsIds from sys.arv(...)
-cmd += str(sys.argv[length-3])
+os.system("echo " + cmd) # pass the download command to the system
+os.system(cmd)
+cmd = ""
+
+## STEP 2 - REDUCE/REPROCESS DATA TO LEVEL=2 ##
+
+# http://cxc.harvard.edu/ciao/threads/createL2/
+# http://cxc.harvard.edu/ciao/ahelp/chandra_repro.html
+
+cmd = "chandra_repro '*' outdir=''" # reprocess all ObsIds, do not save
+                                    # intermediate files
+os.system("echo " + cmd) # pass the reprocess command to the system
+os.system(cmd)
+cmd = ""
+
+## STEP 3 - REPROJECT AND COMBINE ##
+
+# http://cxc.harvard.edu/ciao/threads/merge_all/
+# http://cxc.cfa.harvard.edu/ciao/threads/combine/
+# http://cxc.harvard.edu/ciao/ahelp/punlearn.html
+# http://cxc.harvard.edu/ciao/ahelp/merge_obs.html
+# http://cxc.harvard.edu/ciao/ahelp/reproject_obs.html
+# http://cxc.harvard.edu/ciao/ahelp/flux_obs.html
+
+os.system("punlearn reproject_obs") # restore system default parameter values
+
+cmd = "reproject_obs '*/repro/*evt2*' reproj/" # reproject all ObsIds
 os.system("echo " + cmd)
 os.system(cmd)
 cmd = ""
 
-#Gets the ObsIDs needed: multiple observations as chandra observs in ~50ks chunks. The location of the
-#files have changed in May 2017 and old database is not able to download than use: --mirror 
-#ftp://cda.cfa.harvard.edu/pub along with this command line to download the files from new location.
-#===============================================================================================#
-	
-#================================ 2. REDUCE DATA TO LVL=2 ======================================#
+os.system("punlearn flux_obs")  # restore system default parameter values
 
-for i in range(2, length-2):
-	os.system("echo chandra_repro indir=" + sys.argv[i] + " outdir=" + sys.argv[i] + "/repro cleanup=no") 
-	os.system("chandra_repro indir=" + sys.argv[i] + " outdir=" + sys.argv[i] + "/repro cleanup=no") 
-	#the data to lvl2, stores in repro subdirectory
-
-#Cleanup=no does not remove intermdeiate files
-#More info about processing pipeline at 
-#http://cxc.harvard.edu/ciao/threads/intro_data/index.html#processingversion
-#===============================================================================================#
-
-#============================ 3. CHANGE WCS AND MOSAIC IMAGES ==================================#
-#Default bands parameter is broad: 0.5-7keV (or something)
-
-os.system("punlearn reproject_obs flux_obs")
-
-cmd = "reproject_obs "
-for i in range(2, length-2):
-	zero = ""
-	num0 = 5-len(sys.argv[i])
-	for n in range (0, num0):
-		zero += "0"
-	cmd += (sys.argv[i] + "/repro/acisf" + zero + sys.argv[i] + "_repro_evt2.fits,")
-cmd = cmd[:-1]
-cmd += " reproj/"
-os.system(cmd)
+cmd = "flux_obs 'reproj/*reproj_evt.fits' merged/ bin=0.5" # for GGM filtering
 os.system("echo " + cmd)
-cmd=""
+os.system(cmd)
+cmd = "flux_obs 'reproj/*reproj_evt.fits' merged_2/ bin=2" # for CAS analysis
+os.system("echo " + cmd)                                   # and unsharp mask
+os.system(cmd)
+cmd = ""
 
-os.system("flux_obs reproj/ expcor_mosaic/ bin=0.5")		#for ggm filtering
-os.system("flux_obs reproj/ expcor_mosaic_2/ bin=2") 		#to be used in CAS and unsharp masking
-#===============================================================================================#
+## STEP 4 - CHECK MERGED FILES ARE PRESENT ##
 
-#============================= 4. DEFINE REGION OF EXTRACTION ==================================#
-os.system("python ../reduce/calculator.py " + str(z) + " 70 0.3 0.7 " + str(Rout) + " > data.txt")
+cmd = "python ../reduce/merged_count.py " + cluster
 
-os.chdir('expcor_mosaic_2')
-#os.system("ds9 -log broad_thresh.img &")
-#open broad_thesh.img in ds9 and find central pixel defined by ACCEPT on maintable: find nearest brighest pixel to use as center
+for i in range(4, length) :
+    cmd += (" " + str(sys.argv[i])) # a space-separated list of the ObsIds
 
-#(from ACCEPT datatable) to calculate angular radius of circular region of extraction
+os.system("echo " + cmd) # pass the check command to the system
+os.system(cmd)
+cmd = ""
 
-#create region of extraction and save (in fk5 coordinates) : radius of region typically ~15-20% of cluster Rvir
+## STEP 5 - DEFINE REGION OF INTEREST, MEASURE TOTAL COUNTS, SAVE ROI ##
 
-#name ds9_fk.reg in cluster directory
-#===============================================================================================#
+# http://cxc.harvard.edu/ciao/ahelp/dmmakereg.html
 
-#================================= 5. REMOVE POINT SOURCES =====================================#
-#Only sources very near to xray bright emission are really necessary to remove: only those in region of extraction
-#Make region around point sources in bin=2 broad_thresh.img and save regions in ciao format with physical coords: 
-#make sure region of extraction is not included
+os.system("python ../reduce/ROI_count.py " + cluster +
+          "merged_2/broad_flux.img " + str(redshift) + " " + str(Rout) +
+          " >> cas_process_all_data.py") # determine if cluster has sufficient
+    # data and append quality flag to cas_process_all_data.py
+    # if data is sufficient, create ds9_fk5.reg file, and bk.reg file
 
+## STEP 6 - CLEANUP ##
+
+# do some cleanup, ie. delete unnecessary ObsIds files
+
+os.chdir("..") # go back to the data/ directory
