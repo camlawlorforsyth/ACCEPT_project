d35bdd9dce141d16dea2a8127f2408109a4416c8
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-04-07 21:38:43 -0500

    Update check_coords.py

--- reduction/check_coords.py
+++ reduction/check_coords.py
@@ -1,66 +1,61 @@
 # this script assumes Python 3.5 is in use
 
-'''
-The calling code used in reduce2.py for this file, is of the form:
-python ../reduction/check_coords.py 1E_0657-56
-argv[-]           argv[0]             argv[1]
-'''
-
 # imports
 import os
-import sys
 
-cluster = sys.argv[1] # the cluster name, as indicated
-
-file = open("../issues.txt", "a") # open for writing in data/, appending to the
-                                  # end of the file if it exists
-
-if (not os.path.isfile("ds9_fk5.reg")) :
-    file.write(cluster + " is labelled correctly," +
-               " but 'ds9_fk5.reg' is missing.\n")
-else :
-    newfile = open("ds9_fk5.reg", "r") # open for reading
-    newfile.readline() # read the first line of ds9_fk5.reg
-    f6 = newfile.read(6) # read the first 6 characters
-    if (f6 != "global") :
-        file.write(cluster + "'s ds9_fk5.reg file is saved in the wrong " +
-                   "coordinates.\n")
-    newfile.close()
-
-if (not os.path.isfile("merged_2/bk.reg")) :
-    file.write(cluster + " is labelled correctly," +
-               " but 'bk.reg' is missing.\n")
-else :
-    newfile = open("merged_2/bk.reg", "r")
-    newfile.readline()
-    f6 = newfile.read(6)
-    if (f6 != "circle") :
-        file.write(cluster + "'s bk.reg file is saved in the wrong " +
-                   "coordinates.\n")
-    newfile.close()
-
-if (not os.path.isfile("merged_2/sources_mod.reg")) :
-    file.write(cluster + " is labelled correctly," +
-               " but bin2 'sources_mod.reg' is missing.\n")
-else :
-    newfile = open("merged_2/sources_mod.reg", "r")
-    newfile.readline()
-    f6 = newfile.read(6)
-    if (f6 != "circle") :
-        file.write(cluster + "'s bin2 'sources_mod.reg' file is saved " +
-                   "in the wrong coordinates.\n")
-    newfile.close()
-
-if (not os.path.isfile("merged/sources_mod.reg")) :
-    file.write(cluster + " is labelled correctly," +
-               " but bin0.5 'sources_mod.reg' is missing.\n")
-else :
-    newfile = open("merged/sources_mod.reg", "r")
-    newfile.readline()
-    f6 = newfile.read(6)
-    if (f6 != "circle") :
-        file.write(cluster + "'s bin0.5 'sources_mod.reg' file is saved " +
-                   "in the wrong coordinates.\n")
-    newfile.close()
-
-file.close()
+def main(cluster) :
+    
+    file = open("../issues.txt", "a") # open for writing in data/, appending to
+                                      # the end of the file if it exists
+    
+    if (not os.path.isfile("ds9_fk5.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but 'ds9_fk5.reg' is missing.\n")
+    else :
+        newfile = open("ds9_fk5.reg", "r") # open for reading
+        newfile.readline() # read the first line of ds9_fk5.reg
+        f6 = newfile.read(6) # read the first 6 characters
+        if (f6 != "global") :
+            file.write(cluster + "'s ds9_fk5.reg file is saved in the wrong " +
+                       "coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("merged_2/bk.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but 'bk.reg' is missing.\n")
+    else :
+        newfile = open("merged_2/bk.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bk.reg file is saved in the wrong " +
+                       "coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("merged_2/sources_mod.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but bin2 'sources_mod.reg' is missing.\n")
+    else :
+        newfile = open("merged_2/sources_mod.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bin2 'sources_mod.reg' file is saved " +
+                       "in the wrong coordinates.\n")
+        newfile.close()
+    
+    if (not os.path.isfile("merged/sources_mod.reg")) :
+        file.write(cluster + " is labelled correctly," +
+                   " but bin0.5 'sources_mod.reg' is missing.\n")
+    else :
+        newfile = open("merged/sources_mod.reg", "r")
+        newfile.readline()
+        f6 = newfile.read(6)
+        if (f6 != "circle") :
+            file.write(cluster + "'s bin0.5 'sources_mod.reg' file is saved " +
+                       "in the wrong coordinates.\n")
+        newfile.close()
+    
+    file.close()
+    
+    return
