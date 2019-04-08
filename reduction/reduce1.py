f68d9c9c712d359597006f0fc84a0161a58216c3
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-04-07 21:38:11 -0500

    Update reduce1.py

--- reduction/reduce1.py
+++ reduction/reduce1.py
@@ -5,23 +5,30 @@ For information regarding how this script is initialized, see the 'README.md'
 file in reduction/README.md.
 
 The calling code used in get_all_data.py for this file, is of the form:
-python reduction/reduce1.py 1E_0657-56 0.296 1.1945 554 3184 4984 4985 4986 5355 5356 5357 5358 5361
-argv[-]       argv[0]        argv[1]  argv[2] argv[3] ... argv[N]
+subprocess.run(['python','reduction/reduce1.py','1E_0657-56','104.6234458','-55.94438611','0.296','1.1945','554','3184','4984','4985','4986','5355','5356','5357','5358','5361'])
+                 argv[-]         argv[0]           argv[1]      argv[2]        argv[3]    argv[4]  argv[5] argv[6] ... argv[N]
 '''
 
 # imports
-import os
 import sys
+import subprocess
 
-length = len(sys.argv) # length = 14 for the example above
+import merged_count
+import ROI_count
+
+# constants
+length = len(sys.argv) # length = 16 for the example above
 cluster = sys.argv[1] # the cluster name, as indicated
-redshift = float(sys.argv[2]) # the given redshift
-Rout_Mpc = float(sys.argv[3]) # the maximum outer radius used by Cavagnolo+
+RA = float(sys.argv[2]) # the right ascension of the cluster
+Dec = float(sys.argv[3]) # the declination of the cluster
+redshift = float(sys.argv[4]) # the given redshift
+Rout_Mpc = float(sys.argv[5]) # the maximum outer radius used by Cavagnolo+
 
 ## STEP 0 - CREATE CLUSTER DIRECTORY ##
 
-os.system("mkdir " + cluster) # create a new directory for the cluster
-os.chdir(cluster) # change directory to the cluster directory
+subprocess.run("mkdir " + cluster, shell=True) # create a new directory for the
+                                               # cluster
+subprocess.run("cd " + cluster, shell=True) # change to the cluster directory
 
 ## STEP 1 - OBTAIN DATA FROM THE CHANDRA DATA ARCHIVE ##
 
@@ -31,14 +38,12 @@ os.chdir(cluster) # change directory to the cluster directory
 
 cmd = "download_chandra_obsid " # download Chandra data 
 
-for i in range(4, length-1) :
+for i in range(6, length-1) :
     cmd += str(sys.argv[i]) + "," # creates a comma-separated list of the
                                   # passed ObsIds
 cmd += str(sys.argv[length-1]) + "-q" # append the last ObsId
 
-os.system("echo " + cmd) # pass the download command to the system
-os.system(cmd)
-cmd = ""
+subprocess.run(cmd, shell=True) # pass the download command to the system
 
 ## STEP 2 - REDUCE/REPROCESS DATA TO LEVEL=2 ##
 
@@ -47,9 +52,7 @@ cmd = ""
 
 cmd = "chandra_repro '*' outdir=''" # reprocess all ObsIds, do not save
                                     # intermediate files
-os.system("echo " + cmd) # pass the reprocess command to the system
-os.system(cmd)
-cmd = ""
+subprocess.run(cmd, shell=True) # pass the reprocess command to the system
 
 ## STEP 3 - REPROJECT AND COMBINE ##
 
@@ -60,46 +63,53 @@ cmd = ""
 # http://cxc.harvard.edu/ciao/ahelp/reproject_obs.html
 # http://cxc.harvard.edu/ciao/ahelp/flux_obs.html
 
-os.system("punlearn reproject_obs") # restore system default parameter values
-
+subprocess.run("punlearn reproject_obs", shell=True) # restore system default
+                                                     # parameter values
 cmd = "reproject_obs '*/repro/*evt2*' reproj/" # reproject all ObsIds
-os.system("echo " + cmd)
-os.system(cmd)
-cmd = ""
+subprocess.run(cmd, shell=True)
 
-os.system("punlearn flux_obs")  # restore system default parameter values
-
+subprocess.run("punlearn flux_obs", shell=True)  # restore system default
+                                                 # parameter values
 cmd = "flux_obs 'reproj/*reproj_evt.fits' merged/ bin=0.5 units=time" # for GGM
-os.system("echo " + cmd)                                            # filtering
-os.system(cmd)
+subprocess.run(cmd, shell=True)                                     # filtering
 cmd = "flux_obs 'reproj/*reproj_evt.fits' merged_2/ bin=2 units=time" # for CAS
-os.system("echo " + cmd)                            # analysis and unsharp mask
-os.system(cmd)
-cmd = ""
+subprocess.run(cmd, shell=True)                     # analysis and unsharp mask
 
-## STEP 4 - CHECK MERGED FILES ARE PRESENT ##
+## STEP 4 - CHECK ALL MERGED FILES ARE PRESENT ##
 
-cmd = "python ../reduction/merged_count.py " + cluster
-
-for i in range(4, length) :
-    cmd += (" " + str(sys.argv[i])) # a space-separated list of the ObsIds
-
-os.system("echo " + cmd) # pass the check command to the system
-os.system(cmd)
-cmd = ""
+merged_count.main(cluster, length-6) # prints to data/issues.txt
 
 ## STEP 5 - DEFINE REGION OF INTEREST, MEASURE TOTAL COUNTS, SAVE ROI ##
 
 # http://cxc.harvard.edu/ciao/ahelp/dmmakereg.html
 
-os.system("python ../reduction/ROI_count.py " + cluster +
-          "merged_2/broad_flux.img " + str(redshift) + " " + str(Rout_Mpc) +
-          " >> cas_process_all_data.py") # determine if cluster has sufficient
+quality = ROI_count.main('merged_2/broad_flux.img', RA, Dec, redshift,Rout_Mpc)
+
+with open('cas_process_all_data.py', 'a') as file :
+    file.write("subprocess.run(['python','reduction/reduce2.py','" + cluster +
+               "','" + str(redshift) + "','" + str(Rout_Mpc) + "','" +
+               quality + "'])" )
+
+## STEP 6 - DETECT POINT SOURCES ##
+
+    #================================= 6. POINT SOURCES =====================================#
+#Only sources very near to xray bright emission are really necessary to remove: only those in region of extraction
+#Make region around point sources in bin=2 broad_thresh.img and save regions in ciao format with physical coords: 
+#make sure region of extraction is not included
+
+#if quality == "sufficient" :
+    
+#    subprocess.run("", shell=True)
+    
+
+#os.system("python ../reduction/ROI_count.py " + cluster +
+#          "merged_2/broad_flux.img " + str(redshift) + " " + str(Rout_Mpc) +
+#          " >> cas_process_all_data.py") # determine if cluster has sufficient
     # data and append quality flag to cas_process_all_data.py
     # if data is sufficient, create ds9_fk5.reg file, and bk.reg file
 
-## STEP 6 - CLEANUP ##
+## STEP 7 - CLEANUP ##
 
 # do some cleanup, ie. delete unnecessary ObsIds files
 
-os.chdir("..") # go back to the data/ directory
+subprocess.run("cd ..", shell=True) # go back to the data/ directory
