e946b3662bb8216278a52c8d0baf4062cbd7f1be
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-06-03 22:05:14 -0500

    Update concen_calc.py

--- reduction/concen_calc.py
+++ reduction/concen_calc.py
@@ -1,18 +1,11 @@
 # this script assumes Python 3.5 is in use
 
-'''
-The calling code used in reduce2.py for this file, is of the form:
-python concen_calc.py science_image.fits redshift ROIout
-argv[-]    argv[0]         argv[1]       argv[2]  argv[3]
-'''
-
 # imports
 import numpy as np
 
 from astropy.coordinates import Angle
 from astropy.coordinates import SkyCoord
 from astropy.cosmology import FlatLambdaCDM
-from astropy.io import ascii
 from astropy.io import fits
 import astropy.units as u
 from astropy.wcs import WCS
@@ -21,115 +14,32 @@ from photutils import SkyCircularAperture
 import warnings
 warnings.filterwarnings("ignore") # ignore warnings about WCS unit changes
 
-import sys
-
-# read in the complete catalog
-dat = ascii.read('../../accept_catalog.csv') # requires columns to have unique
-                                             # names
-dat.add_index('z') # index the catalog by redshift so that row queries are
-                  # possible
-
 # constants
 cosmo = FlatLambdaCDM(H0 = 70, Om0 = 0.3) # specify the cosmology being used
 
 #..........................................................................main
-def main() :
-    
-    if len(sys.argv) == 4 :
-        file = sys.argv[1]
-        redshift = float(sys.argv[2])
-        ROIout = float(sys.argv[3])
-    else : # this should only be necessary if not using the automated scripts
-        file = get_image()
-        redshift = get_redshift()
-        ROIout = get_ROIout()
-        print('\nInput confirmed. Continuing with script.\n')
+def main(file, right_ascension, declination, redshift, Rout_Mpc) :
     
     science = fits.open(file) # open the science image
-    #science.info()
     header = science[0].header # define the header, in order to get the WCS
     image = science[0].data # create the image that will be used
     science.close()
     
     world_cs = WCS(header)
     
-    index = dat.loc_indices[redshift] # requires redshift in dat['z'] be unique
-    RA = Angle(dat['RA'][index], u.deg) # get the RA, Dec for this cluster
-    Dec = Angle(dat['Dec'][index], u.deg)
-    R_out = ROIout*u.Mpc # the ROIout for this cluster
+    RA = Angle(right_ascension, u.deg) # the RA, Dec for this cluster
+    Dec = Angle(declination, u.deg)
+    Rout = Rout_Mpc*u.Mpc # the Rout_Mpc for this cluster
     
     D_A = cosmo.angular_diameter_distance(redshift)
-    R_max = (R_out/D_A)*(180/np.pi)*u.deg # to get maximum radius in degrees
+    R_max = (Rout/D_A)*(180/np.pi)*u.deg # maximum radius in degrees
     
     position = SkyCoord(ra=RA, dec=Dec, distance=D_A)
     aperture = SkyCircularAperture(position, r=R_max)
     phot_table = aperture_photometry(image, aperture, wcs=world_cs)
     max_total = phot_table['aperture_sum'][0]
     
-    CC, CC_err = concentration(max_total, R_max, position, image, world_cs)
-    
-    print('{0:g},{1:g} # concen, concen_err'.format(CC, CC_err))
-
-#.....................................................................get_image
-def get_image() :
-    
-    prompt = ('Input the path to the original science image: ')
-    warning = '\b'
-    while warning : # drop out if empty
-        userIn = input(warning + prompt)
-        warning = ''
-        try :
-            val = ( userIn )
-            if val == None :
-                warning = ("You must have some image. Please try again.\n\n")
-            else :
-                pass
-        except ValueError :
-            warning = ("Error. Please try again.\n\n")   
-    
-    return val
-
-#..................................................................get_redshift
-def get_redshift() :
-    
-    prompt = ('Input the unique redshift to use for analysis: ')
-    warning = '\b'
-    while warning : # drop out if empty
-        userIn = input(warning + prompt)
-        warning = ''
-        try :
-            val = float( userIn )
-            if val == None :
-                warning = ("You must have the redshift. Please try again.\n\n")
-            elif val < 0 :
-                warning = ("Redshifts aren't negative. Please try again.\n\n")
-            else :
-                pass
-        except ValueError :
-            warning = ("Error. Please try again.\n\n") 
-    
-    return val
-
-#....................................................................get_ROIout
-def get_ROIout() :
-    
-    prompt = ('Input the ROIout to use for analysis: ')
-    warning = '\b'
-    while warning : # drop out if empty
-        userIn = input(warning + prompt)
-        warning = ''
-        try :
-            val = float( userIn )
-            if val == None :
-                warning = ("You must have an outer radius. Please try again.\n\n")
-            elif val < 0 :
-                warning = ("Radii aren't negative. Please try again.\n\n")
-            else :
-                pass
-        except ValueError :
-            warning = ("Error. Please try again.\n\n") 
-    
-    return val
+    return concentration(max_total, R_max, position, image, world_cs)
 
 #........................................................................search
 def search(desired_total, R_max, position, image, wcs, num_steps) :
@@ -218,5 +128,3 @@ def concentration(max_total, R_max, position, image, wcs) :
     
     return 5*np.log10(r_80 / r_20), uncertainty
 #..............................................................end of functions
-
-main()
